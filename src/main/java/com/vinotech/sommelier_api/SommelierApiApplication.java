b52cab6081008cc044b26fe042e0662d543c1b14
Thierry Duchesne <thierryduchesne95@hotmail.com>
2025-11-13 14:12:21 -0500

    feat(config): Initialisation du backend Spring Boot et configuration de PostgreSQL (profil local)

---
+++ src/main/java/com/vinotech/sommelier_api/SommelierApiApplication.java
@@ -0,0 +1,13 @@
+package com.vinotech.sommelier_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SommelierApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SommelierApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vinotech/sommelier_api/SommelierApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinotech.sommelier_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SommelierApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
